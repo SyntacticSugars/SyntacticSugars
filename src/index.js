dd6421b2f33f879eda3a0feae83890340a6b190b
Tyler Meyerson <tdmeyerson@gmail.com>
2020-01-20 15:24:57 -0500

    added file structure and boilerplate
    Co-authored-by Jenae <jenaepen@gmail.com>
    Co-authored-by Sejan <sejdemi@gmail.com>
    Co-authored-by Vessy <ve33y6@gmail.com>
    Co-authored-by Jason <jsonseidler@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './Components/App.jsx';
+import styles from './styles/master.css';
+
+render(<App/>,document.getElementById('root'));
