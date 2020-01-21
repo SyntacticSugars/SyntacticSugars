8d59bdc5a38127a2808d725beab9164c3001cdf7
Vessy <v.shestorkina@gmail.com>
2020-01-21 18:42:59 -0500

    Created thumbnail button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import ThumbnailButton from './ThumbnailButton.jsx';
 
 class App extends Component {
   // renders a button and a boolean; the button flips the boolean
   render() {
-    return (<h1>App</h1>);
+    const fakeData = {
+      url: 'https://www.biggerbolderbaking.com/wp-content/uploads/2017/09/1C5A0996.jpg',
+      itemName: 'fakeName',
+      itemProducer: 'fakeProducer',
+      itemStars: '4/5',
+      itemPrice: 'cheap',
+      itemNote: 'some note',
+    };
+    return (<ThumbnailButton data={fakeData} />);
   }
 }
 
--- src/components/ThumbnailButton.jsx
+++ src/components/ThumbnailButton.jsx
@@ -1,17 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const ThumbnailButton = (props) => (
 
-  <div className="box" id="thumbnail" onClick="product page route placeholder">
-    <image src={props.url}> image goes here </image>
-    <span id="itemName">{props.itemName}</span>
-    <p>
-      <span className="itemProducer">{props.itemProducer}</span>
-      <span className="itemStars">{props.itemStars}</span>
-      <span className="itemPice">{props.itemPrice}</span>
-      <span className="itemFreeShipping">{props.itemFreeShipping}</span>
-    </p>
-  </div>
-);
+const ThumbnailButton = ({ data }) => {
+  const Name = styled.h1`
+  font-size: 1.5em;
+  color: palevioletred;
+  margin: 2px 0;
+  `;
+  const Info = styled.p`
+  font-size: 1em;
+  color: darkgray;
+  margin: 2px 0;
+  `;
+  const Price = styled.p`
+  font-size: 1em;
+  color: black;
+  font-weight: 600;
+  margin: 2px 0;
+  `;
+  const Note = styled.p`
+  font-size: .75em;
+  color: black;
+  border-style: solid;
+  background-color: palegreen;
+  border-color: palegreen;
+  border-radius: .5em;
+  text-align: center;
+  margin: 0;
+  display: inline-block;
+  padding: .1em;
+  `;
+  const Button = styled.button`
+  padding: .5em;
+  margin: .5em;
+  background-color: gba(250,250,250);
+  box-shadow: 0px 0px 3px rgba(0, 0, 0, .3);  
+  text-align: left;
+  `;
+
+  return (
+    <Button onClick={() => { console.log('Tyler'); }}>
+      <img src={data.url} alt="placeholder" style={{ width: '78px' }} />
+      <Name>{data.itemName}</Name>
+      <Info>{data.itemProducer}</Info>
+      <Info>{data.itemStars}</Info>
+      <Price>{data.itemPrice}</Price>
+      <Note>{data.itemNote}</Note>
+    </Button>
+  );
+};
 
 export default ThumbnailButton;
