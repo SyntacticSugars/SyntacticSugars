2de660897a9d9feb2ea8ce8210778f9589303c44
Jason Seidler <jsonseidler@gmail.com>
2020-01-23 11:45:56 -0500

    modularize routes

--- server/server.js
+++ server/server.js
@@ -7,15 +7,32 @@ const PORT = 3000;
 const app = express();
 const path = require('path');
 const xxxRouter = require('./routes/xRoute.js'); // TEMP ---------------------------------------------
+const authRouter = require('./routes/authRouter.js');
+const authController = require('./controllers/authController.js');
 
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 const passportSetup = require('../config/passport-setup');
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 
+//intialize cookie session
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
+}));
+
+//initialize passport
+app.use(passport.initialize());
+
+//start passport session
+app.use(passport.session());
+
+//connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, { useNewUrlParser: true, useUnifiedTopology: true}, () => {
   console.log('connected to mongodb')
-})
+});
+
 // statically serve everything in the dist folder on the route '/dist'
 app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
@@ -33,24 +50,16 @@ app.get('/', (req, res) => {
 // route handlers
 app.use('/server', xxxRouter); // TEMP ---------------------------------------------
 
-
-
-app.get('/auth/google',
-  passport.authenticate('google', { scope: 
-      ['profile'] }
-));
-
-app.get( '/auth/google/callback', 
-    passport.authenticate( 'google', { 
-        successRedirect: '/auth/google/success',
-        failureRedirect: '/auth/google/failure'
-}));
-
-app.get('/auth/google/success', (req, res) => {
-  res.send('success')
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
 })
-app.get('/auth/google/failure', (req, res) => {
-  res.send('failure')
+
+app.use('/auth', authRouter);
+
+
+app.get('/login', authController.authCheck, (req, res) => {
+  res.redirect('/')
 })
 
 // catch-all route handler for any requests to an unknown route
