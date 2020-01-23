608170ac32119cd7d11898b6a1f063c4d2608c0a
Vessy <v.shestorkina@gmail.com>
2020-01-22 20:55:18 -0500

    useEffect in progress with some errors to fix

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,12 +3,14 @@ const db = require('../models/bakeryModel.js');
 const productController = {};
 
 productController.getProduct = (req, res, next) => {
+  console.log(req.params.id);
   db.query('SELECT * FROM products WHERE _id=$1', [req.params.id], (err, product) => {
     if (err) {
       console.log('err in get product middleware');
       res.status(400);
       next(err);
     }
+    console.log(product);
     res.locals = product;
     return next();
   });
--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 
 const ProductPage = (props) => {
-  // const data = {};
-  const data = {
-    img_url: 'https://www.biggerbolderbaking.com/wp-content/uploads/2017/09/1C5A0996.jpg',
-    id: 1,
-    title: 'fakeName',
-    type: 'Pies',
-    poster_id: 2,
-    rating: 4,
-    price: 'cheap',
-    note: 'some note',
-    description: 'apple',
-    location: 'NYC',
-    shiptime_days: 5,
+  const [data, setData] = useState({});
+  const [error, setError] = useState(false);
+
+  // console.log(window.location.href);
+
+  const getId = () => {
+    const url = window.location.href;
+    const index = url.lastIndexOf(':');
+    const id = url.substring(index + 1);
+    return id;
   };
+  // console.log(getId());
+  function fetchData(id) {
+    fetch(`/product/:${id}`)
+      .then((info) => console.log('KEY WORD', info))
+      .then((info) => setData(info))
+      .catch((err) => {
+        console.log(err);
+        setError(err);
+      });
+  }
+
+  useEffect(() => {
+    fetchData(getId());
+  }, [data]);
+
+
+  // const data = {
+  //   img_url: 'https://www.biggerbolderbaking.com/wp-content/uploads/2017/09/1C5A0996.jpg',
+  //   id: 1,
+  //   title: 'fakeName',
+  //   type: 'Pies',
+  //   poster_id: 2,
+  //   rating: 4,
+  //   price: 'cheap',
+  //   note: 'some note',
+  //   description: 'apple',
+  //   location: 'NYC',
+  //   shiptime_days: 5,
+  // };
+
+  if (error) return (<h1>Product not found!</h1>);
+
   return (
     <div>
       <img src={data.img_url} alt="placeholder" style={{ width: '100%' }} />
