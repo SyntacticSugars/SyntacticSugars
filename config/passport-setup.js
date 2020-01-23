5eddc3435d6bdef898435fa3e97817def3bd2ed1
Jason Seidler <jsonseidler@gmail.com>
2020-01-23 11:44:51 -0500

    oauth config with passport

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,17 @@
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
 const passport = require('passport');
+const User = require('../server/models/userModel.js');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  })
+});
 
 passport.use(new GoogleStrategy({
   clientID: keys.google.clientID,
@@ -9,9 +20,22 @@ passport.use(new GoogleStrategy({
   passReqToCallback   : true
 },
 (req, accessToken, refreshToken, profile, done) => {
-  console.log('profile', profile)
-  User.findOrCreate({ googleId: profile.id }, function (err, user) {
-    return done(err, user);
-  });
-}
-));
+  //check if user already exists in db
+  User.findOne({ googleId: profile.id }).then((currentUser) => {
+    if(currentUser){
+      console.log(currentUser);
+      done(null, currentUser);
+      //already have the user
+    } else {
+      new User({
+        googleId: profile.id,
+        username: profile.displayName,
+        thumbnail: profile._json.picture
+       }).save().then((newUser) => {
+         console.log('new user created:' + newUser)
+         done(null, newUser);
+       })
+    }
+  })
+  })
+);
