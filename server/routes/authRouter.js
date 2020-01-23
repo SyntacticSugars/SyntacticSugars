fdb80b605daf92ea520e4bf7d22b50dc5f0bfc20
Jason Seidler <jsonseidler@gmail.com>
2020-01-23 11:47:37 -0500

    add authRouter

---
+++ server/routes/authRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const passport = require('passport');
+const authRouter = express.Router();
+
+authRouter.get('/google',
+  passport.authenticate('google', { scope: 
+      ['profile'] }
+));
+
+authRouter.get( '/google/callback', passport.authenticate( 'google'), (req, res) => {
+  res.redirect('/login');
+});
+
+module.exports = authRouter;
